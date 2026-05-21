a1ce2a0f06c803d3045f647252be099a4f0af04b
attila-laszlo-hajdo <hajdolaszloattila@gmail.com>
2026-05-21 06:58:34 +0300

    Add todo UI and basic task functionality - adding items to the list function

---
+++ script.js
@@ -0,0 +1,13 @@
+const input = document.getElementById("taskInput");
+const button = document.getElementById("addBtn");
+const taskList = document.getElementById("taskList");
+
+button.addEventListener("click", function(){
+
+  const task = input.value;
+  const li = document.createElement("li");
+  li.textContent = task;
+  taskList.appendChild(li);
+  input.value = "";
+  
+});
